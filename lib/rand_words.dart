7d106b1acdd21dd8dfd0a1f411a30d6a112b2abe
Gonzalo Berhouet <gonzalo.berhouet@ecs.co.uk>
2022-01-28 14:01:48 +0000

    favorites

--- lib/rand_words.dart
+++ lib/rand_words.dart
@@ -10,10 +10,12 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _randomWordPairs = <WordPair>[];
+  final _savedWordPairs = <WordPair>{};
 
   Widget _buildList() {
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
+        // iteration build list of widgets
         itemBuilder: (context, item) {
           if (item.isOdd) return const Divider();
 
@@ -27,14 +29,48 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
     return ListTile(
-        title: Text(pair.asPascalCase, style: const TextStyle(fontSize: 18.0)));
+        title: Text(pair.asPascalCase, style: const TextStyle(fontSize: 18.0)),
+        trailing: Icon(alreadySaved ? Icons.face_rounded : Icons.face_outlined,
+            color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _savedWordPairs.remove(pair);
+            } else {
+              _savedWordPairs.add(pair);
+            }
+          });
+        });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      final Iterable<ListTile> tiles = _savedWordPairs.map((WordPair pair) {
+        return ListTile(
+            title: Text(pair.asPascalCase,
+                style: const TextStyle(fontSize: 16.0)));
+      });
+
+      final List<Widget> divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+      return Scaffold(
+          appBar: AppBar(title: const Text('Saved Words')),
+          body: ListView(children: divided));
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('WordPair Generator')),
+        appBar: AppBar(
+          title: const Text('WordPair Generator'),
+          actions: <Widget>[
+            IconButton(onPressed: _pushSaved, icon: const Icon(Icons.list))
+          ],
+        ),
         body: _buildList());
   }
 }
